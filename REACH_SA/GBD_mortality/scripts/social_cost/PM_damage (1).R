093e3251d3e47a766a0dfae5d1608b9a707a50d0
Medinat Akindele <84250137+medinata@users.noreply.github.com>
2025-09-01 13:06:23 -0500

    Delete REACH_SA/GBD_mortality directory

--- REACH_SA/GBD_mortality/scripts/social_cost/PM_damage (1).R
+++
@@ -1,6 +0,0 @@
-
-poll_info <- data.frame(poll_var = 'PM25-PRI',
-                        species = 'PM')
-
-
-source(paste0(getwd(), '/GBD_mortality/scripts/social_cost/Other_damages_GBD.R'))
--- REACH_SA/GBD_mortality/scripts/social_cost/VOC_damage.R
+++
@@ -1,6 +0,0 @@
-
-poll_info <- data.frame(poll_var = 'VOC',
-                        species =  'OC_secondary')
-
-
-source(paste0(getwd(), '/GBD_mortality/scripts/social_cost/Other_damages_GBD.R'))
