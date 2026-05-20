5e35452527ad381b70976d4ca261967d5771b2cf
Medinat Akindele <84250137+medinata@users.noreply.github.com>
2026-05-20 09:54:59 -0500

    Add files via upload

--- REACH_SA/scripts/concentrations/calibration_coefficients.R
+++ REACH_SA/scripts/concentrations/calibration_coefficients.R
@@ -1,7 +1,7 @@
 # REACH-US calibration coefficients
-
+#' 5/4/26: corrected NOX oxidation scale, prior S-R script was reading SO2 oxidation timescale due to incorrect notation
 SO4_cal <- 1.23
-NOX_cal <- 0.5
+NOX_cal <- 0.35
 NH3_cal <- 0.3
 OC_cal <- 0.024
 PM_cal <- 0.34
